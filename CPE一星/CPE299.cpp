60c3d7c24768797804ce1a9953d2d24e44e51800
justin725817 <justinlee3324@gmail.com>
2024-03-13 00:28:25 +0800

    test

--- CPE一星/CPE299.cpp
+++ CPE一星/CPE299.cpp
@@ -1,31 +1,32 @@
-// HARD
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main (){
+int main()
+{
     cin.sync_with_stdio(0);
     cin.tie(0);
     int N;
     cin >> N;
-    for (int i=0;i<N;i++){
+    for (int i = 0; i < N; i++)
+    {
         int L;
         cin >> L;
-        vector<int> train;
+        vector<int> num;
         for (int j = 0; j < L; j++)
         {
-            int v;
-            cin >> v;
-            train.push_back(v);
+            int tmp;
+            cin >> tmp;
+            num.push_back(tmp);
         }
         int k = 0;
         for (int j = 0; j < L; j++)
         {
             for (int k = 0; k < L - 1; k++)
             {
-                if (train[k] > train[k + 1])
+                if (num[k] > num[k + 1])
                 {
-                    swap(train[k], train[k + 1]);
+                    swap(num[k], num[k + 1]);
                     k++;
                 }
             }
