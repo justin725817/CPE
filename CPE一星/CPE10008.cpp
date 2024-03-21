fdf4bd0d5d53e7e3c243163d1137fab728943373
justin725817 <justinlee3324@gmail.com>
2024-03-22 00:51:01 +0800

    update

---
+++ CPE一星/CPE10008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    string lett;
+    for (int i=0;i<num;i++){
+        cin>>lett;
+    }
+    
+    
+}
